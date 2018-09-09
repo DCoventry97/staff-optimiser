8bf2f27b552be1434365419836e4807cd1f9030f
Dan Coventry <daniel.coventry2@gmail.com>
2018-09-09 12:41:27 +0100

    web page gets current time and selected score

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -6,14 +6,17 @@ $(document).ready(function(){
     $("#staff-selector").submit(function (e){
         e.preventDefault();
         alert("Starting Session For: " +$("#staff-id-input").val());
-        staff_id = $("#staff-id-input").val();
-        console.log(staff_id);
+        let staff_id = $("#staff-id-input").val();
         $(this).hide()
         $("#score-selector").show();
     })
 
     $(".rating-button").click(function() {
         alert("Thank you for feedback");
+        let selected_score_button = $(this).attr("id");
+        let score = selected_score_button[4];
+        let time = new Date();
+        let hour = time.getHours();
     })
 
 })
