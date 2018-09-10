e254c91087d8b08dd340336ef4e9b464e5ef66b0
Dan Coventry <daniel.coventry2@gmail.com>
2018-09-10 18:08:46 +0100

    add comments to main.py

--- src/server/rest/main.py
+++ src/server/rest/main.py
@@ -17,6 +17,7 @@ def index():
 
 @app.route("/getreport/<id>")
 def view_report(id):
+    #calculates advice and sends it to the user
     result = lin_reg.get_val(id)
     if abs(result) > 0.1:
         advice = "No trend detected, they are consistent over the day."
@@ -26,21 +27,24 @@ def view_report(id):
         advice = "more productive earlier in the day."
     return render_template("result.html", advice=advice, id=id)
 
-
+#page for manager to view all staff in system and request advice for each of them. 
 @app.route("/getreport", methods=["GET","POST"])
 def get_report():
     if request.method == "GET":
+        #make list of names of staff in system
         all_names = []
         tree = ET.parse("./staff.xml")
         root = tree.getroot()
         for child in root:
             all_names.append(child.text)
+            #send page with names of all staff in system
         return render_template("select_report.html", names=all_names)
     else:
+        #redirect user to requested report page
         staff = request.form["staff-select"]
         return redirect("/getreport/"+staff)
 
-
+#post method to send staff score to. 
 @app.route("/staffscore", methods=["POST"])
 def staff_score():
     staff_name = ""
@@ -55,33 +59,35 @@ def staff_score():
         time = subdata.get("time")
         score = subdata.get("score")
 
+        #check if staff member has an xml file, makes one if not
         file = path.isfile("./xml/"+staff_name+".xml")
         if not file:
             root = ET.Element("staffData")
             tree = ET.ElementTree(root)
             tree.write("./xml/"+staff_name+".xml")
             
+            #makes a file to store all staff names in if it does not exist
             if not path.isfile("./staff.xml"):
                 root = ET.Element("staffNames")
                 tree = ET.ElementTree(root)
                 tree.write("staff.xml")
 
+            #stores staff members name in staff.xml
             tree = ET.parse("./staff.xml")
             root = tree.getroot()
-            print("ppppppppppppppppp")
             new_user = ET.SubElement(root, "staff")
             new_user.text = staff_name
             tree.write("staff.xml")
 
-        with open("./xml/"+staff_name+".xml") as file:
-            tree = ET.parse("./xml/"+staff_name+".xml")
-            root = tree.getroot()
-            new_rating = ET.SubElement(root, "rating")
-            new_time = ET.SubElement(new_rating, "time")
-            new_time.text = str(time)
-            new_score = ET.SubElement(new_rating, "score")
-            new_score.text = str(score)
-            tree.write("./xml/"+staff_name+".xml")
+        #adds new score and time for staff member
+        tree = ET.parse("./xml/"+staff_name+".xml")
+        root = tree.getroot()
+        new_rating = ET.SubElement(root, "rating")
+        new_time = ET.SubElement(new_rating, "time")
+        new_time.text = str(time)
+        new_score = ET.SubElement(new_rating, "score")
+        new_score.text = str(score)
+        tree.write("./xml/"+staff_name+".xml")
 
         return jsonify({"a":score}), 201
 
